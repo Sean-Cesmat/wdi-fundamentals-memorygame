91ba0766d5b9923dd551e13da6ed0951fab6b065
Sean Cesmat <sean.cesmat@gmail.com>
2018-01-17 11:29:32 -0800

    Finished Game And Bonus

--- js/main.js
+++ js/main.js
@@ -21,24 +21,54 @@ var cards = [
 }
 ];
 var cardsInPlay = [];
+var score = 0;
 var checkForMatch = function() {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
+			score++;
+			document.getElementById('score').textContent = score;
 			alert('You found a match!!');
 		}
 		else {
 			alert('Sorry, try again.');
 		}
 	}
-}
-var flipCard = function(cardId) {
+};
+var flipCard = function() {
+	cardId = this.getAttribute("data-id");
 	console.log('User flipped ' + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 	
-}
-flipCard(0);
-flipCard(2);
+};
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+
+	}
+};
+
+var clearBoard = function() {
+	for (var i = 0; i < cards.length; i++){
+		document.getElementsByTagName('img')[i].setAttribute('src', 'images/back.png');
+		cardsInPlay = [];
+	}
+};	
+var resetButton = document.getElementsByClassName('reset-button')[0];
+resetButton.addEventListener('click', clearBoard);
+
+createBoard();
+
+
+// flipCard(0);
+// flipCard(2);
 
